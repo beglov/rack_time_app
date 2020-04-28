95c2e2f6dabde504da70b6549613d76dd1e37cab
Beglov Sergey <beglov.sergey@yandex.ru>
2020-04-28 11:03:58 +0300

    rack current time and date service

--- app.rb
+++ app.rb
@@ -1,52 +1,38 @@
 require_relative 'time_formatter'
 
 class App
-  AVAILABLE_FORMAT = %w[year month day hour minute second].freeze
-
   def call(env)
     @env = env
 
-    [status, headers, body]
+    if time_path?
+      handle_params
+    else
+      response(404, 'Not Found')
+    end
   end
 
   private
-
-  def status
-    return 404 unless time_path?
-    return 400 if unknown_format?
-
-    200
-  end
-
-  def headers
-    {'Content-Type' => 'text/plain'}
-  end
-
-  def body
-    return [] unless time_path?
-    return ["Unknown time format [#{unknown_format.join(', ')}]\n"] if unknown_format?
-
-    ["#{TimeFormatter.new.format(format_array)}\n"]
-  end
 
   def time_path?
     @env['PATH_INFO'] == '/time'
   end
 
-  def unknown_format?
-    !unknown_format.empty?
+  def response(status, body)
+    Rack::Response.new(
+      body,
+      status,
+      {'Content-Type' => 'text/plain'}
+    ).finish
   end
 
-  def unknown_format
-    format_array - AVAILABLE_FORMAT
-  end
+  def handle_params
+    formatter = TimeFormatter.new(params['format'])
 
-  def format_array
-    format.split(',')
-  end
-
-  def format
-    params['format']
+    if formatter.valid?
+      response(200, formatter.time)
+    else
+      response(400, formatter.errors)
+    end
   end
 
   def params
--- time_formatter.rb
+++ time_formatter.rb
@@ -1,4 +1,6 @@
 class TimeFormatter
+  AVAILABLE_FORMAT = %w[year month day hour minute second].freeze
+
   FORMAT_TO_STRFTIME = {
     'year' => '%Y',
     'month' => '%m',
@@ -8,12 +10,26 @@ class TimeFormatter
     'second' => '%S',
   }.freeze
 
-  def initialize(time = Time.now)
+  def initialize(format, time = Time.now)
+    @received_formats = format.split(',')
     @time = time
   end
 
-  def format(args)
-    template = args.map { |i| FORMAT_TO_STRFTIME[i] }.join('-')
-    @time.strftime(template)
+  def valid?
+    return false if @received_formats.empty?
+
+    @unknown_formats = @received_formats - AVAILABLE_FORMAT
+    @unknown_formats.empty?
+  end
+
+  def time
+    template = @received_formats.map { |i| FORMAT_TO_STRFTIME[i] }.join('-')
+    "#{@time.strftime(template)}\n"
+  end
+
+  def errors
+    return "Specify format!\n" if @received_formats.empty?
+
+    "Unknown time format [#{@unknown_formats.join(', ')}]\n"
   end
 end
